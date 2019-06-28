4f9a2911c4c972d19efd5fbe9d56f9cd1fe1da13
Daniel Alves <daniel.zancheta@hotmail.com>
2019-06-28 09:24:45 -0400

    Criacao do Projeto

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Quantos vendedores gostaria de adicionar? ");
+            int qtdVendedor = int.Parse(Console.ReadLine());
+
+            List<Vendedor> lista = new List<Vendedor>();
+
+            for (int i = 0; i < qtdVendedor; i++)
+            {
+                Console.WriteLine($"Vendedor #{i + 1}");
+                
+                Console.Write("Id: ");
+                int id = int.Parse(Console.ReadLine());
+                Console.Write("Nome: ");
+                string nome = Console.ReadLine();
+                Console.Write("Salario: ");
+                double salario = double.Parse(Console.ReadLine(), NumberFormatInfo.InvariantInfo);
+
+                Vendedor vendedor = new Vendedor(id, nome, salario);
+                lista.Add(vendedor);
+                Console.WriteLine();
+            }
+
+            Console.Write("Entre com o ID do vendedor que tera o salário aumentado: ");
+            int idAumentoSalario = int.Parse(Console.ReadLine());
+
+            Console.Write("Entre com o percentual de aumento: ");
+            double percentualAumento = double.Parse(Console.ReadLine(), NumberFormatInfo.InvariantInfo);
+
+            Console.WriteLine();
+
+            Vendedor vendedorAumento = new Vendedor();
+            vendedorAumento = lista.Find(x => x.Id == idAumentoSalario);
+            vendedorAumento.AumentaSalario(vendedorAumento, percentualAumento);
+
+            Console.WriteLine("Lista atualizada dos funcionários: ");
+            foreach (var vendedor in lista)
+            {
+                Console.WriteLine(vendedor);
+            }
+
+            Console.WriteLine("Linha para teste");
+            string teste = Console.ReadLine();
+        }
+    }
+}
---
+++ ConsoleApp1/Vendedor.cs
@@ -0,0 +1,32 @@
+﻿namespace ConsoleApp1
+{
+    class Vendedor
+    {
+        public int Id { get; private set; }
+        public string Nome { get; private set; }
+        public double Salario { get; private set; }
+
+        public Vendedor(int id, string nome, double salario)
+        {
+            Id = id;
+            Nome = nome;
+            Salario = salario;
+        }
+
+        public Vendedor()
+        {
+
+        }
+
+        public void AumentaSalario(Vendedor vendedor, double percentual)
+        {
+            double acrecimo = vendedor.Salario * (percentual/100.00);
+            vendedor.Salario += acrecimo;
+        }
+
+        public override string ToString()
+        {
+            return $"{Id}, {Nome}, {Salario}";
+        }
+    }
+}
